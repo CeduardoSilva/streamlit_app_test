cdac754d8009c173df56295a74a72b58172f80a0
Carlos Eduardo da Silva <cs.eduardo@icloud.com>
2025-02-16 18:19:09 -0300

    updates deps

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import joblib as jl
 import plotly.express as px
+from prophet import Prophet
 
 # Se quiser apenas st.line_chart, pode usar matplotlib ou streamlit nativo
 
